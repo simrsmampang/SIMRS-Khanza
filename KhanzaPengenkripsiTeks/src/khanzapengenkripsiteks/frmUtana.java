ad084d4a4d46f5c60dd94917085b201d918ee946
Muhammad Rizky Rizaldi <myquinn489@gmail.com>
2023-05-29 15:54:19 +0800

    Fix build error

--- KhanzaPengenkripsiTeks/src/khanzapengenkripsiteks/frmUtana.java
+++ KhanzaPengenkripsiTeks/src/khanzapengenkripsiteks/frmUtana.java
@@ -129,6 +129,7 @@ public class frmUtana extends javax.swing.JFrame {
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
+            @Override
             public void run() {
                 new frmUtana().setVisible(true);
             }
