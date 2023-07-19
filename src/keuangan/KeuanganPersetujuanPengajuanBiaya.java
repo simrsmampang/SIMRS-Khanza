721a2322c2a6f76cc0488179e5c63cae5dabf828
mas-elkhanza <khanza_media@yahoo.com>
2023-07-19 09:23:49 +0700

    Belum Fix, Persetujuan Pengajuan Biaya & Pemantauan EWS Neonatus

--- src/keuangan/KeuanganPersetujuanPengajuanBiaya.java
+++ src/keuangan/KeuanganPersetujuanPengajuanBiaya.java
@@ -746,6 +746,7 @@ private void BtnCariKeyPressed(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_B
                             tabMode.removeRow(tbBangsal.getSelectedRow());
                             hitung();
                     }
+                    DlgPersetujuan.dispose();
                 }   
             }else{
                 JOptionPane.showMessageDialog(null,"Silahkan pilih No.Pengajuan yang diajukan..!!");
